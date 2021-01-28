de2cde1a753ebff89310a6126938de2983eaa618
Heather Stratton <hastratton@gmail.com>
2021-01-28 13:04:29 -0500

    updated fromHeather

--- HelloFromAriel/src/rojas/Main.java
+++ HelloFromAriel/src/rojas/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Ariel");
+        System.out.println("Hello, Ariel. My name is Heather.");
+
     }
 }
